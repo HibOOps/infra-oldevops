a12e55b74b440c908fb92ca6088fc78d9301c7e8
HibOOps <olivier.labe59100@gmail.com>
2025-10-14 12:24:07 +0200

    ok

--- terraform/backup.tf
+++ terraform/backup.tf
@@ -1,18 +1,14 @@
 
 terraform {
-    backend "s3" {           
+    backend "s3" {
     bucket                      = "infraoldevops"
     key                         = "terraform/homelab/terraform.tfstate"
     region                      = "rbx"
-    profile = "oldevops"
-    endpoints = {
-      s3 = "https://s3.rbx.io.cloud.ovh.net/"
-    }                       
+    profile                     = "oldevops"
+    endpoint                    = "https://s3.rbx.io.cloud.ovh.net/"
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
-    skip_s3_checksum            = true
-    skip_requesting_account_id  = true
     }
 }
 
